f317fd78edc2ccfe48f182173c1adc8d8275c66d
larissa4lves <p4ndap0wer24@gmail.com>
2022-08-22 21:39:40 -0300

    update

---
+++ 14miniProjetos/scriptFundamentos.js
@@ -0,0 +1,93 @@
+// ****VARIAVEIS****
+
+// let idade = 5;
+// console.log(idade);
+// let altura = 1.73
+// console.log(altura);
+// camelCase
+// let nome = 'Larissa';
+// console.log(nome);
+
+// *** EXERCICIO1 IMPRIMIR NA TELA TRES VARIAVEIS DIFERENTES ***
+
+// let nome = 'Larissa';
+// console.log(nome);
+
+// let idade = 26;
+// console.log(idade);
+
+// let dataDeNascimento = 15; 
+// console.log(dataDeNascimento);
+//______________________________________________________________________________________________________________________________
+// **** CONSTANTES ****
+// RETRIBUIÇÃO DE VALORES
+
+// const valorIngressoAdulto = 20;
+// console.log(valorIngressoAdulto);
+//______________________________________________________________________________________________________________________________
+// **** TIPOS PRIMITIVOS ****
+
+// let nome = 'Rafael';// string literal
+// let idade = 25; // number literal
+// let estaAprovado = true; // boolean
+// let sobrenome = undefined; // Undefined
+// let corSelecionado = null; //Redefinir um valor
+//______________________________________________________________________________________________________________________________
+// **** OBJETOS ****
+
+//*** EXERCICIO2 COLOCAR MAIS TRÊS PROPRIEDADES ***
+// let pessoa = {
+//     nome: 'Rafael',
+//     idade: 25,
+//     estaAprovado: true,
+//     sobrenome: 'Borges',
+//     altura: 1.73,
+//     peso: 85.0,
+//     apelido: 'Rafa'
+
+// };
+
+// ====pulaLinha==== //
+
+// console.log(pessoa);
+
+// let coisasFavoritas = {
+//     cor: 'azul',
+//     comida: 'strogonoof',
+//     bebida: 'agua',
+//     lugar: 'cama',
+//     numero: 4
+// };
+
+// console.log(coisasFavoritas);
+
+// ______________________________________________________________________________________________________________________________
+// **** ARRAYS ****
+
+// exercicios mostrando algumas funções
+
+//                0, 1, 2, 3
+// let familia = [26,45,50,18];
+// console.log(familia);
+
+// ====pulaLinha==== //
+
+// let familia = [true,45,'jhonatan',18];
+// console.log(familia);
+
+// ====pulaLinha==== //
+
+// let familia = [true,45,'jhonatan',18];
+// console.log(familia.length);
+// console.log(familia[0]);
+
+// ====pulaLinha==== //
+
+// let nomeDoAmigo = ['frassete',28,'rio'];
+// console.log(nomeDoAmigo.length);
+// console.log(nomeDoAmigo);
+
+//______________________________________________________________________________________________________________________________
+// **** FUNCTIONS ****
+
+
