9d476c9376b97b9f387dcb9d9c07b02e99b614c3
larissa4lves <p4ndap0wer24@gmail.com>
2022-08-25 00:20:58 -0300

    update

--- 14miniProjetos/scriptFundamentos.js
+++ 14miniProjetos/scriptFundamentos.js
@@ -90,4 +90,126 @@
 //______________________________________________________________________________________________________________________________
 // **** FUNCTIONS ****
 
+// Verbo ++ Substantivo 
+
+// let corSite = "azul";
+// function resetaCor(cor, tonalidade){
+//     corSite = cor + '  ' + tonalidade;
+// }
+// console.log(corSite);
+// resetaCor("verde", "claro");
+// console.log(corSite);
+
+//______________________________________________________________________________________________________________________________
+// **** TIPOS DE fUNÇÃO ****
+
+// Realiza uma tarefa e não devolve nada
+
+// function dizerNome(){
+//     console.log('Larissa');
+// }
+
+// dizerNome();
+
+// // Faz um calculo ou operação e retorna algo 
+// function multiplicadorPorDois(valor){
+//     return valor *2;
+// }
+
+// let resultado = multiplicadorPorDois(5);
+
+// console.log(resultado);
+
+//______________________________________________________________________________________________________________________________
+// **** Operadores aritiméticos no Javascript ****
+// let salario = 100;
+
+// // +, -, *, /, **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** 5);
+
+// let idade = 18;
+// console.log(--idade);
+// console.log(idade);
+//______________________________________________________________________________________________________________________________
+// ****  Operadores de atribuição no Javascript  ****
+
+// let valorTecladoGamer = 10;
+// valorTecladoGamer = valorTecladoGamer + valorTecladoGamer;
+// valorTecladoGamer = valorTecladoGamer - valorTecladoGamer;
+// valorTecladoGamer -= valorTecladoGamer;
+// console.log(valorTecladoGamer);
+// ______________________________________________________________________________________________________________________________
+//  **** Operadores de igualdade no Javascript ****
+// operadores de igualdade estrita
+// console.log (1 === 1);
+// console.log ('1' === 1);
+
+// Igualdade solta
+// console.log (1 == 1);
+// console.log ('1' == 1);
+//______________________________________________________________________________________________________________________________
+// **** Operador Ternário ****
+//  let pontos = 200;
+//  let tipo = pontos > 100 ? 'premium': 'comum';
+//  console.log(tipo);
+//______________________________________________________________________________________________________________________________
+// **** Operadores Lógicos e (&&) ****
+// retorna TRUE se os dois operandos forem TRUE
+
+// console.log(true && false);
+
+// Operadores Lógicos e (||)
+// retorna TRUE se um dos operandos forem TRUE
+
+// let maiorDeIdade = false;
+// let possuiCarteiraDeTrabalho = true;
+// let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+
+// console.log(podeAplicar);
+
+// let maiorDeIdade = true;
+// let possuiCarteiraDeTrabalho = true;
+// let podeAplicar = maiorDeIdade && possuiCarteiraDeTrabalho;
+
+// console.log('podeAplicar', podeAplicar);
+
+//Operador NOT (!)
+// let candidatoRecusado = !podeAplicar;
+
+// console.log('Candidato Recusado', candidatoRecusado);
+
+//______________________________________________________________________________________________________________________________
+// **** Comparações não boleanos ****
+
+// falsy
+
+// undefined
+// null
+// 0
+// false
+// ''
+// // NaN - not a number
+
+// Truthy
+
+let corPersonalidade = 'Vermelho';
+let corPadrao = 'Azul';
+let corPerfil = corPersonalizada || corPadrao;
+
+console.log(corPerfil);
+
+
+
+
+
+
+
+
+
+
 
